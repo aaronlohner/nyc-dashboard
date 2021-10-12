d60e07676caa69e9d72e99c4818e9a5eb50230c5
alohne <aaron.lohner@mail.mcgill.ca>
2021-10-11 21:01:09 -0400

    update main, fix to preprocess

--- nyc_dash/main.py
+++ nyc_dash/main.py
@@ -1,7 +1,7 @@
 import os.path as osp
 import pandas as pd
 import json
-from bokeh.layouts import column, row
+from bokeh.layouts import column
 from bokeh.models import Div, Dropdown, ColumnDataSource
 from bokeh.plotting import figure, curdoc
 
@@ -9,20 +9,17 @@ from bokeh.plotting import figure, curdoc
 dirname = osp.dirname(__file__)
 data_path = osp.join(dirname, 'zipcodes.json')
 zipcodes = json.load(open(data_path,))
-zipcodes_menu = list(zipcodes.keys())
-
-# Constants
-monthly_avg_incident_times = zipcodes['all']
-zip1_default = '10000'
-zip2_default = '10001'
-monthly_avg_incident_times_z1 = zipcodes[zip1_default]
-monthly_avg_incident_times_z2 = zipcodes[zip2_default]
+zipcodes_menu = list(zipcodes.keys())[1:] # exclude 'all' (the first key) from dropdown menus
+zipcodes_menu.sort()
+zip1_default = zipcodes_menu[0]
+zip2_default = zipcodes_menu[1]
 
 # Widgets
 dropdown_z1 = Dropdown(label="Zipcode 1", button_type="primary", menu=zipcodes_menu)
 dropdown_z2 = Dropdown(label="Zipcode 2", button_type="warning", menu=zipcodes_menu)
 text_z1 = Div(text=f"<p>Zipcode 1: {zip1_default}</p>""", width=300, height=30,)
 text_z2 = Div(text=f"<p>Zipcode 2: {zip2_default}</p>""", width=300, height=30,)
+text_login = Div(text=f"<p>Please enter the correct username and password as parameters in the URL to login.</p>""",)
 
 # Build plot
 p = figure(toolbar_location=None, title='Monthly average incident create-to-close time', x_axis_type='datetime',
@@ -31,9 +28,9 @@ p = figure(toolbar_location=None, title='Monthly average incident create-to-clos
 # Build plot data
 months = pd.date_range('2020-01-01','2020-12-31', freq='MS').tolist()
 data={'Month' : months,
-        'All' : monthly_avg_incident_times,
-        'z1' : monthly_avg_incident_times_z1,
-        'z2' : monthly_avg_incident_times_z2}
+        'All' : zipcodes['all'],
+        'z1' : zipcodes[zip1_default],
+        'z2' : zipcodes[zip2_default]}
 source = ColumnDataSource(data=data)
 
 # Plot the data
@@ -50,7 +47,6 @@ def update_zipcode2(event):
         source.data['z2'] = zipcodes[event.item]
         text_z2.text = "Zipcode 2: " + event.item
         
-
 dropdown_z1.on_click(update_zipcode1)
 dropdown_z2.on_click(update_zipcode2)
 
@@ -59,4 +55,4 @@ if (len(url) == 2 and 'username' in url.keys() and 'password' in url.keys() and
         url['username'][0].decode('utf-8') == 'nyc' and url['password'][0].decode('utf-8') == 'iheartnyc'):
         curdoc().add_root(column(dropdown_z1, text_z1, dropdown_z2, text_z2, p))
 else:
-        curdoc()
+        curdoc().add_root(text_login)
--- nyc_dash/preprocess.py
+++ nyc_dash/preprocess.py
@@ -1,51 +1,59 @@
-from numpy.core.fromnumeric import mean
-import pandas as pd
 import os.path as osp
-from pandas._libs.tslibs.timedeltas import Timedelta
+import pandas as pd
+from numpy.core.fromnumeric import mean
 import json
 
+dirname = osp.dirname(__file__)
+
 def load_data(datafile:str, headerfile:str) -> pd.DataFrame:
-    dirname = osp.dirname(__file__)
-    data_path = osp.join(dirname, datafile)
+    data_path = osp.join(dirname, '..', 'data', datafile)
     header_path = osp.join(dirname, headerfile)
     header = [line.rstrip() for line in open(header_path)]
-    return pd.read_csv(data_path, usecols=['Created Date', 'Closed Date', 'Incident Zip'], names=header)#, nrows=1_000)
+    return pd.read_csv(data_path, usecols=['Created Date', 'Closed Date', 'Incident Zip'], names=header,
+                        dtype={'Incident Zip':'str'}, parse_dates=['Created Date', 'Closed Date'], nrows=100)
 
 def clean_data(df:pd.DataFrame) -> pd.DataFrame:
     # Drop NAs (zipcodes, closed dates)
     df.dropna(inplace=True, subset=['Closed Date', 'Incident Zip'])
-    df['Incident Zip'] = pd.to_numeric(df['Incident Zip'], downcast='integer') #NEEDED?
-
     # Remove negative time differences
-    df[['Created Date', 'Closed Date']] = df[['Created Date', 'Closed Date']].apply(pd.to_datetime)
     df = df[df[['Created Date', 'Closed Date']].apply(lambda x : x[1] - x[0] > pd.Timedelta(0), axis=1)]
     return df
 
-def monthly_avg_incident_time(df:pd.DataFrame, month_num:int, zip:int=None) -> float:
-    df = df[df['Created Date'].dt.month == month_num]
-    if zip is not None:
+def monthly_avg_incident_time(df:pd.DataFrame, month_num:int, zip:str=None) -> float:
+    df = df[df['Closed Date'].dt.month == month_num]
+    if zip is not None: # only None when looking at all zipcodes
         df = df[df['Incident Zip'] == zip]
-    if df.shape[0] == 0:
-        return 0 # TEMPORARY MEASURE
+    if df.shape[0] == 0: # if no incidents resolved in a month, set avg resolve time to 0
+        return 0
     diff = df[['Created Date', 'Closed Date']].apply(lambda x : pd.Timedelta(x[1] - x[0]), axis=1)
     return float(mean(diff).seconds/3600)
 
-def monthly_avg_incident_times(df:pd.DataFrame, zip:int=None):
+def monthly_avg_incident_times(df:pd.DataFrame, zip:str=None):
     return [monthly_avg_incident_time(df, m, zip) for m in range(1, 13)]
 
 
 def write_json_output(df:pd.DataFrame, out:str):
     zipcodes = df['Incident Zip'].unique()
-    #zipcodes.sort()
     json_dict = {}
     json_dict["all"] = monthly_avg_incident_times(df)
     for zip in zipcodes:
-        json_dict[str(zip)] = monthly_avg_incident_times(df, zip)
+        json_dict[zip] = monthly_avg_incident_times(df, zip)
 
     json_obj = json.dumps(json_dict, indent = 4)
     with open(out, "w") as output:
         output.write(json_obj)
 
+
 if __name__ == '__main__':
-    df = clean_data(load_data('nyc_311_limit_2020.csv', 'header.txt'))
-    #write_json_output(df, 'zipcodes.json')
+    df = clean_data(load_data('nyc_311_limit_trimmed.csv', 'header.txt'))
+    #write_json_output(df, osp.join(dirname, 'zipcodes.json'))
+    diff = df[['Created Date', 'Closed Date']].apply(lambda x : pd.Timedelta(x[1] - x[0]), axis=1)
+    s = 0
+    for d in diff:
+        print(d)
+        s = s + d.seconds
+    #print(diff.shape[0])
+    print(s)
+    print(s/diff.shape[0])
+    print(mean(diff).seconds)
+    
